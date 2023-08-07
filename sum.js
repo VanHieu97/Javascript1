5366eaff78d0a3a3f3496da32815d08ddf79e876
VanHieu97 <vanhieucv1997@gmail.com>
2023-08-07 13:46:08 +0700

    Link exercise

---
+++ sum.js
@@ -0,0 +1,9 @@
+function SUM () {
+    let a = +document.getElementById('G').value;
+    let b = +document.getElementById("H").value;
+    let c = +document.getElementById("J").value;
+    let d = +document.getElementById("K").value;
+    let e = a + b + c + d;
+    alert(e);
+    console.log(e);
+}
